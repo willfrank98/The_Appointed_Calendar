d8e47b2534ce8a8632e3b32b90aba0ebd388ca79
Will Frank <willfrank98@gmail.com>
2019-11-20 18:13:46 -0700

    seperate js file

---
+++ CalendarScheduler/wwwroot/js/callendar.js
@@ -0,0 +1,11 @@
+﻿<script>
+	document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+		plugins: [ 'dayGrid' ]
+  });
+
+  calendar.render();
+});
+</script>
