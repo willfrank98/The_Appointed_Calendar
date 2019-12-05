5ae78bed87fca74801a6c1fddb8650a50c3a0305
Will Frank <willfrank98@gmail.com>
2019-12-05 00:01:36 -0700

    removed unused method

--- CalendarScheduler/wwwroot/js/callendar.js
+++ CalendarScheduler/wwwroot/js/callendar.js
@@ -1,33 +1,6 @@
 ﻿var calendar;
 
 document.addEventListener('DOMContentLoaded', function () {
-	jQuery.fn.extend({
-		getPath: function () {
-			var path, node = this;
-			while (node.length) {
-				var realNode = node[0], name = realNode.localName;
-				if (!name) break;
-				name = name.toLowerCase();
-
-				var parent = node.parent();
-
-				var sameTagSiblings = parent.children(name);
-				if (sameTagSiblings.length > 1) {
-					var allSiblings = parent.children();
-					var index = allSiblings.index(realNode) + 1;
-					if (index > 1) {
-						name += ':nth-child(' + index + ')';
-					}
-				}
-
-				path = name + (path ? '>' + path : '');
-				node = parent;
-			}
-
-			return path;
-		}
-	});
-
 	var calendarEl = document.getElementById('calendar');
 
 	calendar = new FullCalendar.Calendar(calendarEl, {
